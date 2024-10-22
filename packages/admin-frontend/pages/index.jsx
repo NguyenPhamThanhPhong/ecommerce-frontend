642c8915c251f1f51afcd60f978e2208b8d6477e
thanhphong17160@gmail.com <thanhphong17160@gmail.com>
2024-10-22 21:06:45 +0700

    save before it never (done setup next admin)

---
+++ packages/admin-frontend/pages/index.jsx
@@ -0,0 +1,13 @@
+// pages/index.jsx
+import React from 'react';
+import RecipeReviewCard from '@shared/RecipeReviewCard';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <RecipeReviewCard>Click me!</RecipeReviewCard>
+    </div>
+  );
+};
+
+export default HomePage;
